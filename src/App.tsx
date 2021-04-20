b78f91381e2841ae91d4fe3bb56887147da2faee
Daniel San Juan Abrante <dani.sanjuan.93@gmail.com>
2021-04-20 16:53:40 +0100

    Add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import React from 'react';
+import "react-image-gallery/styles/css/image-gallery.css";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import "react-image-gallery/styles/css/image-gallery.css";
 import './App.css';
 import DogSelector from './components/autocomplete/dogSelector.component';
+import './i18n';
 import { setupInterceptorsTo } from './interceptor/axios.interceptor';
 
 const App = (): JSX.Element => {
