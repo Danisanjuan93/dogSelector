6b7b72fd8346eee55efe0f0b19266735f9dc9f5d
Daniel San Juan Abrante <dani.sanjuan.93@gmail.com>
2021-04-20 22:41:41 +0100

    Add label when dog has dog breed. Update some functional logic

--- src/components/carousel/dogBreedCarousel.component.tsx
+++ src/components/carousel/dogBreedCarousel.component.tsx
@@ -3,7 +3,7 @@ import ImageGallery, { ReactImageGalleryProps } from 'react-image-gallery';
 import { DogsImages } from "../../interfaces/dog.interface";
 import "./dogBreedCarousel.style.css";
 
-const DogBreedCarousel = (props: { images: DogsImages[] }): JSX.Element => {
+const DogBreedCarousel = (props: { images: DogsImages }): JSX.Element => {
 
     const [thumbnailPosition, setThumbnailPosition] = React.useState<"top" | "right" | "bottom" | "left" | undefined>("bottom");
 
@@ -12,7 +12,7 @@ const DogBreedCarousel = (props: { images: DogsImages[] }): JSX.Element => {
     }
 
     const options: ReactImageGalleryProps = {
-        items: props.images,
+        items: props.images.images,
         disableThumbnailScroll: true,
         showPlayButton: false,
         thumbnailPosition: thumbnailPosition,
@@ -20,7 +20,12 @@ const DogBreedCarousel = (props: { images: DogsImages[] }): JSX.Element => {
     }
 
     return (
-        <ImageGallery {...options} />
+        <>
+            <div className="align-breeds-text">
+                {props.images.breeds.length > 0 && <h3>Mostrando: {props.images.breeds.join(', ')}</h3>}
+            </div>
+            <ImageGallery {...options} />
+        </>
     )
 
 }
